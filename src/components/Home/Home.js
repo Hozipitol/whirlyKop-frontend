05a8508d791e6dbdeb83427e3e4e467d1482ed42
Swarnim55 <72058504+Swarnim55@users.noreply.github.com>
2022-03-22 15:03:54 +0545

    homenew

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Home1 from './Home1'
 import Home2 from './Home2'
+import Home2b from './Home2b'
 import Home3 from './Home3'
 import Home4 from './Home4'
 import Home5 from './Home5'
@@ -11,6 +12,7 @@ function Home() {
     <div>
     <Home1 />
     <Home2 />
+    <Home2b/>
     <Home3 />
     <Home4 />
     <Home5 />
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import './Home2.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import wrld from '../../images/modern-design.jpg'
 function Home2() {
   return (
-    <div id="about" className="block aboutBlock">
+    <div id="about" className="cov block aboutBlock">
       <div className="container-fluid">
-        <div className="titleHolder">
-          <h2>Attractive Oppurtunities in the Edtech and smart classroom market</h2>
+        <div className="row titleHolder">
+          <span style={{fontSize:"xx-large", margin:"0% auto"}}>Attractive Oppourtunities in the Edtech and smart classroom market</span>
           <p></p>
         </div>
-        <div className="contentHolder">
-<h1>The global Edtech and samrt classroom market is expected to reach USD 181.3billion by 2025 growing at CAGR is 16.1% during the forecast period </h1></div>
+       <br/>
+     
+      
+        <div className="row">
+          <div className="col" style={{padding:"2%"}}>
+          <span style={{fontSize:"xx-large"}}>85.8 </span>
+          <span style={{fontSize:"large"}}>USD Billion 2020-e </span>
+          <div className="progress">
+          
+  <div className="progress-bar progress-bar-striped bg-dark progress-bar-animated" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style={{width: "20%"}}></div>
+</div>
+          </div>
+          <div className="col" style={{padding:"2%"}}>
+          <span style={{fontSize:"xx-large"}}>181.3 </span>
+          <span style={{fontSize:"large"}}>USD Billion 2025-p </span>
+          <div className="progress">
+          
+  <div className="progress-bar progress-bar-striped bg-warning progress-bar-animated" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{width: "60%"}}></div>
+</div>
+          </div>
+        </div>
+     
+<span style={{fontSize:"x-large", padding:"2%", margin:"0% auto"}}>The global Edtech and smart classroom market is expected to reach USD 181.3 billion by 2025 growing at CAGR is 16.1% during the forecast period </span>
+   
         
-      </div>
+
+        </div>
+       
     </div>
   )
 }
