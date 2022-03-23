a9022df569d679ee01b74111c8acf3b573f2e1af
Hozipitol <huzefapitol67@gmail.com>
2022-03-23 17:50:41 +0530

    img

--- src/components/Home/Home2b.js
+++ src/components/Home/Home2b.js
@@ -7,7 +7,7 @@ function Home2b() {
       <div className="conmap" >
       <br/>
           <div className="row" style={{ margin:"0% auto"}}>
-        <img src={wrld} style={{maxWidth:"500px", margin:"0 auto"}} height="200" alt="asd"  />
+        {/* <img src={wrld} style={{maxWidth:"500px", margin:"0 auto"}} height="200" alt="asd"  /> */}
         <span style={{maxWidth: "75%",fontSize:"x-large", textAlign:"center", wordBreak:"break-word", padding:"2%", margin: "0% auto"}}>    The market growth in APAC can be attributed to the presence of a large number of EdTech Startups and the huge demand for eLearning in the region. </span>
         </div>
       </div>
